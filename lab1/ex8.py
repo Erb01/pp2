c0b88b7ac13b077970a9f0f1d2c994700ac556ec
Erbol <tald1774@gmail.com>
2025-01-19 15:10:33 +0500

    lab1

---
+++ lab1/ex8.py
@@ -0,0 +1,7 @@
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+d = str("s1") # d will be 's1'
+e = str(2)    # e will be '2'
+u = str(3.0)  # u will be '3.0'
